70e4b74c868ca2e57e2d7b04f0923666a1a4c7d6
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2014-06-25 08:05:49 +0000

    Modified import of trap recaptures to assign them a final time of day (23:59:59) so that they fall out in the correct order w.r.t. antenna data.

--- data_table_stage/form_data_trap_captures.R
+++ data_table_stage/form_data_trap_captures.R
@@ -9,6 +9,13 @@ trap_recaptures <- unique(trap_recaptures[
 				trap_recaptures[['tag']],
 				trap_recaptures[['detection_date']]),])
 
+## Trap recaptures typically have no date/time but they are the last
+## time a fish might be seen so we give them a final time of day:
+
+hour(trap_recaptures[['detection_date']]) <- 23
+minute(trap_recaptures[['detection_date']]) <- 59
+second(trap_recaptures[['detection_date']]) <- 59
+
 dbWriteTable(link$conn, 'data_trap_recaptures', trap_recaptures,
 						 row.names=FALSE, overwrite=TRUE, append=FALSE)
 
